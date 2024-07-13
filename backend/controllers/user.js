9cedf1b732a659c8b5b16c0ca46e84eccdf3f65c
Fatih Illeez <101408078+Fatih-Illeez@users.noreply.github.com>
2024-07-12 23:42:07 -0400

    changes made

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,7 @@
 const User = require("../models/user");
+const EmailVerificationToken = require("../models/emailVerificationToken");
+const nodemailer = require("nodemailer");
+const { isValidObjectId } = require("mongoose");
 
 exports.create = async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,14 +13,53 @@ exports.create = async (req, res) => {
   const newUser = new User({ name, email, password });
   await newUser.save();
 
-  // var transport = nodemailer.createTransport({
-  //   host: "live.smtp.mailtrap.io",
-  //   port: 587,
-  //   auth: {
-  //     user: "api",
-  //     pass: "9e74388e24467a9d96dccb5d7464443a",
-  //   },
-  // });
+  // generate 6 digit otp
+  let OTP = "";
+  for (let i = 0; i < 5; i++) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+  // store otp inside our db
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: newUser._id,
+    token: OTP,
+  });
+  //send that otp to our user
 
-  res.status(201).json({ user: newUser });
+  await newEmailVerificationToken.save();
+
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "b5b903729887b8",
+      pass: "ab77261f625edd",
+    },
+  });
+
+  transport.sendMail({
+    from: "verification@reviewapp.com",
+    to: newUser.email,
+    subject: "Email Verification",
+    html: `
+      <h1>Your verification OTP</h1>
+      <p>Hi ${newUser.name},</p>
+      <p>Thank you for registering. Please use the following OTP to verify your email.</p>
+      <h2>${OTP}</h2>`,
+  });
+
+  res.status(201).json({ message: "User created successfully" });
+};
+
+exports.verifyEmail = async (req, res) => {
+  const { userId, OTP } = req.body;
+
+  if (isValidObjectId(userId)) return res.json({ error: "Invalid user" });
+
+  const user = await User.findById(userId);
+  if (!user) return res.json({ error: "User not found" });
+  if (user.isVerified) return res.json({ error: "user is already verified" });
+
+  const token = await EmailVerificationToken.findOne({ owner: user._id });
+  if (!token) return res.json({ error: "token not found" });
 };
